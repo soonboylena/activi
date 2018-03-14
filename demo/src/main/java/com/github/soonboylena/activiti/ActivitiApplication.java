040497a2c3ff5e60c6c37bf4ac591ced68244820
sunb <soonboylena@gmail.com>
2018-03-14 14:20:18 +0800

    pom import

---
+++ demo/src/main/java/com/github/soonboylena/activiti/ActivitiApplication.java
@@ -0,0 +1,53 @@
+package com.github.soonboylena.activiti;
+
+import org.activiti.engine.IdentityService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.identity.Group;
+import org.activiti.engine.identity.User;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+public class ActivitiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitiApplication.class, args);
+
+    }
+
+
+    @Bean
+    CommandLineRunner init(final RuntimeService runtimeService) {
+
+        return strings -> {
+            Map<String, Object> variables = new HashMap<>();
+            variables.put("applicantName", "John Doe");
+            variables.put("email", "john.doe@activiti.com");
+            variables.put("phoneNumber", "123456789");
+            runtimeService.startProcessInstanceByKey("hireProcess", variables);
+        };
+    }
+
+    @Bean
+    InitializingBean usersAndGroupsInitializer(final IdentityService identityService) {
+
+        return () -> {
+
+            Group group = identityService.newGroup("user");
+            group.setName("users");
+            group.setType("security-role");
+            identityService.saveGroup(group);
+
+            User admin = identityService.newUser("admin");
+            admin.setPassword("admin");
+            identityService.saveUser(admin);
+
+        };
+    }
+}
---
+++ demo/src/main/java/com/github/soonboylena/activiti/component/ResumeService.java
@@ -0,0 +1,10 @@
+package com.github.soonboylena.activiti.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ResumeService {
+    public void storeResume() {
+        System.out.println("Storing resume ...");
+    }
+}
