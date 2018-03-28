81e18d4bfbf0a9d1d2dac83c670d7ad6f36f9d09
sunb <soonboylena@gmail.com>
2018-03-28 22:52:15 +0800

    加了一个模块：neo4j

---
+++ persistent-neo4j/src/main/java/com/github/soonboylena/persistentneo4j/PersistentNeo4jApplication.java
@@ -0,0 +1,12 @@
+package com.github.soonboylena.persistentneo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistentNeo4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistentNeo4jApplication.class, args);
+	}
+}
---
+++ persistent-neo4j/src/test/java/com/github/soonboylena/persistentneo4j/PersistentNeo4jApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.soonboylena.persistentneo4j;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PersistentNeo4jApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
