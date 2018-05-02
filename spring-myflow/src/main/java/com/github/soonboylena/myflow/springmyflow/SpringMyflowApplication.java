481d6ca514b2999d7b0bcb91cbf34b04fe18236e
sunb <soonboylena@gmail.com>
2018-05-02 18:25:57 +0800

    从角色权限入手，考虑通过spring无侵入的实现扩展

---
+++ spring-myflow/src/main/java/com/github/soonboylena/myflow/springmyflow/SpringMyflowApplication.java
@@ -0,0 +1,12 @@
+package com.github.soonboylena.myflow.springmyflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMyflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMyflowApplication.class, args);
+	}
+}
